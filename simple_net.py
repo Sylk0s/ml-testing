7f01e3921478eeed69e7fa390ab067b8abb7c0f9
Jake K <68135816+Sylk0s@users.noreply.github.com>
2021-12-30 14:56:05 -0800

    creates the net without error

--- simple_net.py
+++ simple_net.py
@@ -19,17 +19,17 @@ class Node:
     # I don't completly understand why bias exists so im just gonna ignore it for now
     def __init__(self, layer):
         self.layer = layer
-        self.input_weights = [random() for _ in self.input_nodes] # init with random intial weights
         self.activation = 0
 
     # I'm chosing to do these init functions AFTER creation so that the network has info about it's structure
     def init_node(self):
-        if self.previous is None:
+        if self.layer.previous is None:
             pass
             # handles something else here leter
             # this means the layer is the first layer
         else:
-            self.input_nodes = self.previous.nodes
+            self.input_nodes = self.layer.previous.nodes
+            self.input_weights = [random.random() for _ in self.input_nodes] # init with random intial weights
 
     # calculates the activation values for each node
     # this is the sum of the input activations times the weights
@@ -43,7 +43,7 @@ class NetLayer:
 
     def init_layer(self, previous):
         self.previous = previous
-        for i in len(self.nodes):
+        for i in range(len(self.nodes)):
             self.nodes[i].init_node()
 
 class NeuralNet:
@@ -51,15 +51,21 @@ class NeuralNet:
         self.layers = [NetLayer(layer_sizes[s], self) for s in range(len(layer_sizes))]
 
         # passes the previous layer to each layer, is None is layer is the input layer
-        for i in len(self.layers):
+        for i in range(len(self.layers)):
             try:
-                layer.init_layer(self.layers[i-1])
+                self.layers[i].init_layer(self.layers[i-1])
             except IndexError:
-                layer.init_layer(None)
+                self.layers[i].init_layer(None)
             
 
 layers = 3
 layer_sizes = [2,4,2]
 
-net = NeuralNet()
-net.init_layers(layers, layer_sizes)
+net = NeuralNet(layer_sizes)
+
+# It seems like the first layer is getting input weights which i think is correct but im not sure why its doing this
+for layer in net.layers:
+    lstr = ""
+    for node in layer.nodes:
+        lstr += str(node.input_weights)
+    print(lstr+"\n")
