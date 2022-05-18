6d8cd87c7e4bf489ba0ffd8ecfc22aac8fc43a2f
Sylk0s <sylkos49@gmail.com>
2022-05-18 08:44:32 -0400

    Forward propegation logic 'works'

--- ex3.py
+++ ex3.py
@@ -1,28 +1,37 @@
 import numpy as np
+import random as rand
 
 class Layer:
-    def __init__(size):
-        size = size
-        weights = []
-        bias = []
+    def __init__(self, size, prev, net):
+        self.net = net
+        self.size = size
+        self.weights = [[rand.random() for _ in range(prev)] for _ in range(size)]
+        self.bias = 0
+        self.prev = prev
+
+    def __str__(self):
+        return f"Size: {self.size}, Weights: {self.weights}, Bias: {self.bias}"
         
-    def activation(v):
+    def activation(self):
         return v #f(v) whatever f may be
 
     # for each layer transforms the data
     # returns a vector of the transformed data
-    def fp(input_data):
-        return [np.dot(n, weights) for n in input_data] + bias
+    def fp(self, input_data):
+        return [np.dot(input_data, self.weights[n])+self.bias for n in range(self.size)]
 
-    def bp():
+    def bp(self):
         pass
 
 class Net:
-    def __init__(rate):
-        layers = []
-        learning_rate = rate
+    # rate = learning rate of the network
+    # sizes must include an array with the first element as the input data size,
+    # the last element as the output size, and all inbetween the sizes of the layers desired
+    def __init__(self, sizes, rate=0.1):
+        self.layers = [Layer(sizes[s],(sizes[0] if s==0 else sizes[s-1]),self) for s in range(len(sizes))]
+        self.learning_rate = rate
 
-    def error(r,s):
+    def error(self, r,s):
         pass
 
     def forward(self,data):
@@ -35,7 +44,7 @@ class Net:
         for layer in reversed(self.layers):
             error = layer.bp(error)
     
-    def train(dataset, n):
+    def train(self, dataset, n):
         for _ in range(n):
             # [0][i] must have the same length as [1][i]
             # these represent inputs and results
@@ -46,3 +55,8 @@ class Net:
                 solution = dataset[1][i]
                 result = self.forward(data_in)
                 self.back(result, solution)
+
+net = Net([4,4,2])
+print([f"{j}: {net.layers[j]}" for j in range(len(net.layers))])
+
+print(net.forward([0,0,0,1]))
